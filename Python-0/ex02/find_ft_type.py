b6deb690c5c0783654d3684c637c2ae3917f77f9
hckforpeace <pierbey@gmail.com>
2025-12-14 22:37:07 +0100

    ex02

---
+++ Python-0/ex02/find_ft_type.py
@@ -0,0 +1,16 @@
+def all_thing_is_obj(object: any) -> int:
+    t = type(object)
+
+    if t.__name__ == "list":
+        print(t.__name__.capitalize(), ":", t)
+    elif t.__name__ == "tuple":
+        print(t.__name__.capitalize(), ":", t)
+    elif t.__name__ == "set":
+        print(t.__name__.capitalize(), ":", t)
+    elif t.__name__ == "dict":
+        print(t.__name__.capitalize(), ":", t)
+    elif t.__name__ == "str":
+        print(object, ":", t)
+    else:
+        print("Type not found")
+    return 42
