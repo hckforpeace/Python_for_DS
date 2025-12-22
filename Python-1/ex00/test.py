8e39203e1335928f9607c3f42220a3a3a9aafdc0
hckforpeace <pierbey@gmail.com>
2025-12-22 18:11:52 +0100

    assertion error

--- Python-1/ex00/test.py
+++ Python-1/ex00/test.py
@@ -1,7 +1,10 @@
 from give_bmi import give_bmi, apply_limit
 
-height = [2.71, 1.15]
+height = [2.71, 1.15, 23]
 weight = [165.3, 38.4]
-bmi = give_bmi(height, weight)
-print(bmi, type(bmi))
-print(apply_limit(bmi, 26))
+try :
+    bmi = give_bmi(height, weight)
+    print(bmi, type(bmi))
+    print(apply_limit(bmi, 26))
+except AssertionError as error:
+    print("AssertionError:", error)
